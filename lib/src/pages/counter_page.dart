daa4f207dbe3beddc5e19f4946cdfe849f18b947
Lucas Zanek <lucaszanek94@gmail.com>
2020-05-27 08:59:11 -0300

    Added clear all and remove element with validation for negative numbers

--- lib/src/pages/counter_page.dart
+++ lib/src/pages/counter_page.dart
@@ -8,39 +8,68 @@ class CounterPage extends StatefulWidget {
 class _CounterPageState extends State<CounterPage> {
   final TextStyle _textStyle = new TextStyle(fontSize: 25);
 
-  int _counter = 10;
+  int _counter = 0;
 
   @override
   Widget build(context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Lucas App"),
-        centerTitle: true,
-        backgroundColor: Color.fromRGBO(400, 345, 222, 34),
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "Numero de clicks:",
-            style: _textStyle,
-          ),
-          Text('$_counter', style: _textStyle),
-        ],
-      )),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Color.fromRGBO(400, 345, 222, 34),
-        onPressed: () {
-          setState(() {
-            _counter++;
-          });
-        },
-        child: Center(
-          child: Icon(Icons.add),
+        appBar: AppBar(
+          title: Text("Lucas App"),
+          centerTitle: true,
+          backgroundColor: Color.fromRGBO(400, 345, 222, 34),
         ),
-      ),
-    );
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Numero de clicks:",
+              style: _textStyle,
+            ),
+            Text('$_counter', style: _textStyle),
+          ],
+        )),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FloatingActionButton(
+              backgroundColor: Color.fromRGBO(400, 345, 222, 34),
+              onPressed: () {
+                setState(() {
+                  if (_counter < 1) {
+                    return;
+                  }
+                  _counter--;
+                });
+              },
+              child: Center(
+                child: Icon(Icons.remove),
+              ),
+            ),
+            FloatingActionButton(
+              backgroundColor: Color.fromRGBO(400, 345, 222, 34),
+              onPressed: () {
+                setState(() {
+                  _counter++;
+                });
+              },
+              child: Center(
+                child: Icon(Icons.add),
+              ),
+            ),
+            FloatingActionButton(
+              backgroundColor: Color.fromRGBO(400, 345, 222, 34),
+              onPressed: () {
+                setState(() {
+                  _counter = 0;
+                });
+              },
+              child: Center(
+                child: Icon(Icons.undo),
+              ),
+            ),
+          ],
+        ));
   }
 }
